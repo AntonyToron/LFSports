1326381f8e1869141a83f19c017d02cab703d2a0
Antony Toron <atoron@princeton.edu>
2017-01-05 14:53:27 -0500

    updates to commenting on backend

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
 	var ENTER_KEY = 13;
-
+	var index = 0;
 	// load comments for forum
 	$.get('http://localhost:8081/get_comments', function(response, status) {
 			var responseParsed = JSON.parse(response);
 			$("#comment_section").append(responseParsed.comment);
+
+			// can add delay
+			$("#comment_section").animate({ scrollTop: $('#comment_section').prop("scrollHeight")}, 0);
 		}
 	);
+
+	// scroll to bottom of div
+	// $("#comment_section").scrollTop($("#comment_section")[0].scrollHeight);
 
 
 	//  append comment
 	$("#type_comment").on('keyup', function(event) {
 		if (event.keyCode == ENTER_KEY) {
 			// gets value of form elements like input, select, textarea
+			$("#type_comment").blur();
 			var comment = $("#type_comment").val();
 			$("#type_comment").val("");
 			// $("#comment_section").append("<p>" + comment + "<p>");		
@@ -22,8 +29,12 @@ $(document).ready(function() {
 				"userName": "Antony Toron",
 				"forumType": "Boxing"
 			}
+			var object = {
+				"comment": comment,
+				"requestType": "POST",
+			}
 
-			connection.send(JSON.stringify(comment));
+			connection.send(JSON.stringify(object));
 		}
 	});
 
@@ -33,16 +44,68 @@ $(document).ready(function() {
 		console.log("connected on client side");
 	};
 
+
+
 	connection.onmessage = function(message) {
-		var data = JSON.parse(message.data);
-		$("#comment_section").append(
-			"<div style='border: 1px solid black; padding: 10px'>" + 
-			"<p>" + data.userName + "<p>" + 
-			"<p>" + data.timeStamp + ": " + data.comment + "<p>"
-			+ "</div>"
-			);
+		// can later change this to class media
+		var rawData = JSON.parse(message.data);
+		if (rawData.requestType == "POST") {
+			var data = rawData.comment;
+			var index = rawData.index;
+			var commentDiv = $("<div></div");
+			commentDiv.attr("id", index);
+			var commentName = $("<p></p>").text(data.userName);
+			var comment = $("<p></p>").text(data.timeStamp + ": " + data.comment);
+			var closeButton = $("<button></button>");
+			var innerSpan = $("<span></span>").html("&times;");
+			closeButton.attr("aria-label", "Close");
+			closeButton.attr("data-dismiss", "modal");
+			closeButton.attr("type", "button");
+			closeButton.attr("id", index);
+			var timeStamp = data.timeStamp;
+
+			innerSpan.attr("aria-hidden", "true");
+			closeButton.addClass("close");
+			closeButton.append(innerSpan);
+			
+			commentDiv.addClass("well");
+			commentDiv.append(closeButton);
+			commentDiv.append(commentName);
+			commentDiv.append(comment);
+			$("#comment_section").append(commentDiv);
+			// commentDiv.insertBefore("#type_comment");
+			// scroll back down to element
+			$("#comment_section").animate
+			({ scrollTop: $('#comment_section').prop("scrollHeight")}, 1000);
+		}
+		else if (rawData.requestType == "DELETE") {
+			var element = $("#" + rawData.index);
+			element.hide(300, function() {element.remove();});
+			console.log("successful delete");
+		}
+		// $("#comment_section").append(
+		// 	"<div class='well'>" + 
+		// 	"<p>" + data.userName + "<p>" + 
+		// 	"<p>" + data.timeStamp + ": " + data.comment + "<p>"
+		// 	+ "</div>"
+		// 	);
 
 	};
+
+
+	//  for deleting comments
+	$(document.body).on('click', 'button', function(event) {
+		if (this.class="close") {
+			var element = $("#" + this.id);
+			element.hide(300, function() {element.remove();});
+			var object = {
+				"index" : this.id,
+				"requestType": "DELETE",
+				"forumType": "Boxing"
+			}
+			connection.send(JSON.stringify(object));
+		}
+	});
 	//  going to have to use ajax load for comments
 	//  maybe on lifecycle of 1 second or 3 seconds? 
 	//  only update those differences between current state
@@ -51,5 +114,28 @@ $(document).ready(function() {
 	// 	event.preventDefault();
 	// 	$("#comment_section").append("<p>Random Text<p>");
 	// });
+	// NEXT STEP - add index in backend for an every 
+			// comment in a forum for easy deletion, and implement
+			// deletion on backend.
+			// closeButton.click(function(event) {
+			// 	$("#" + this.id).remove();
+			// 	console.log(this);
+			// 	console.log(event);
+			// 	// $.ajax({
+			// 	// 	url: "http://localhost:8081/delete_comment",
+			// 	// 	type: 'POST',
+			// 	// 	crossDomain: true,
+			// 	// 	dataType: 'jsonp',
+			// 	// 	data: {
+			// 	// 		"index": this.id
+			// 	// 	},
+			// 	// });
+			// 	var object = {
+			// 		"index" : this.id,
+			// 		"requestType": "DELETE",
+			// 		"forumType": "Boxing"
+			// 	}
+			// 	connection.send(JSON.stringify(object));
+			// });
 
 });
--- websocket-server/websocket.js
+++ websocket-server/websocket.js
@@ -24,25 +24,64 @@ app.get('/get_comments', function(request, response) {
 	if (obj == undefined)
 		comment = "";
 	else {
-		for (var i = 0; i < obj.length; i++) {
-			comment += "<div \
-			style='border: 1px solid black; padding: 10px'>" + 
-			"<p>" + obj[i].userName + "<p>" + 
-			"<p>" + obj[i].timeStamp + ": " 
-			+ obj[i].comment + "<p>" +
+		object = obj.commentArray;
+
+		// for (var i = 0; i < obj.length; i++) {
+		// 	console.log(obj[i].index);
+		// 	comment += "<div id='" + obj[i].index + "' \
+		// 	class='well'>" + 
+		// 	"<button type='button' id='" + obj[i].index + "' aria-label='Close' data-dismiss='modal' class='close'>" +
+		// 	"<span aria-hidden='true'>&times</span>" + 
+		// 	"</button>" +
+		// 	"<p>" + obj[i].userName + "</p>" + 
+		// 	"<p>" + obj[i].timeStamp + ": " 
+		// 	+ obj[i].comment + "</p>" +
+		// 	"</div>" ;
+		// }
+		// console.log(object);
+		var length = Object.keys(object).length;
+		var keys = Object.keys(object);
+		// console.log(keys);
+		for (var i = 0; i < keys.length; i++) {
+			// console.log(object);
+			// console.log(object.keys[i]);
+			var temp = keys[i];
+			comment += "<div id='" + object[temp].index + "' \
+			class='well'>" + 
+			"<button type='button' id='" + object[temp].index + "' aria-label='Close' data-dismiss='modal' class='close'>" +
+			"<span aria-hidden='true'>&times</span>" + 
+			"</button>" +
+			"<p>" + object[temp].userName + "</p>" + 
+			"<p>" + object[temp].timeStamp + ": " 
+			+ object[temp].comment + "</p>" +
 			"</div>" ;
 		}
 	}
 
 	response.send(JSON.stringify({
-		"comment": comment
+		"comment": comment,
 	}));
 });
+
+// app.get('/delete_comment', function(request, response) {
+// 	response.header("Access-Control-Allow-Origin", "*");
+//   	response.header("Access-Control-Allow-Headers", "Session, Origin, X-Requested-With, Content-Type, Accept");
+//   	var obj = forums['Boxing'];
+//   	if (obj == undefined)
+//   		console.log("trying to delete a comment that does not exist");
+
+//   	var index = request.query.index;
+//   	console.log(index);
+
+//   	obj.commentArray()
+
+//   	// obj.splice
+//   	// response.send("Successful delete");
+// });
 
 var server = app.listen(8081, function() {
 	console.log("express listening...");
 })
-
 
 
 
@@ -68,18 +107,59 @@ wsServer.on('request', function(request) {
 		if (message.type === 'utf8') {
 			// maybe should be pushing only the string format here
 			// so less expensive on client side
-			var comment = JSON.parse(message.utf8Data);
-			if (forums[comment.forumType] == undefined) {
-				var commentArray = [];
-				commentArray.push(comment);
-				forums[comment.forumType] = commentArray;
+			var data = JSON.parse(message.utf8Data);
+			if (data.requestType == "POST") {
+				var comment = data.comment;
+				if (forums[comment.forumType] == undefined) {
+					var commentArray = {};
+					comment.index = 0;
+					// commentArray.push(comment);
+					commentArray[comment.index] = comment;
+					forums[comment.forumType] = {
+						"commentArray": commentArray,
+						"amountOfComments": 1
+					}
+				}
+				else {
+					comment.index = forums[comment.forumType].amountOfComments;
+					// console.log(comment.index);
+					// forums[comment.forumType].commentArray.push(comment);
+					forums[comment.forumType].commentArray[comment.index] = comment;
+					forums[comment.forumType].amountOfComments++;
+				}
+				//  send this new comment to only those that are on the forum accordingly
+				for (var i = 0; i < clients.length; i++) {
+					clients[i].sendUTF(JSON.stringify({
+						"comment": comment,
+						"index": forums[comment.forumType].amountOfComments - 1,
+						"requestType": "POST"
+					}))
+				}
 			}
-			else {
-				forums[comment.forumType].push(comment);
-			}
-			//  send this new comment to only those that are on the forum accordingly
-			for (var i = 0; i < clients.length; i++) {
-				clients[i].sendUTF(JSON.stringify(comment))
+			else if (data.requestType == "DELETE") {
+				var forumType = data.forumType;
+				var index = data.index;
+				var array = forums[forumType].commentArray;
+				if (array.length == 1)
+					forums[forumType].commentArray = {};
+				else {
+					// console.log(forums[forumType].commentArray);
+					// forums[forumType].commentArray.splice(index, 1);
+					// console.log(forums[forumType].commentArray);
+					// console.log(forums[forumType]);
+					delete forums[forumType].commentArray[index];
+
+										// console.log(forums[forumType]);
+
+				}
+
+				for (var i = 0; i < clients.length; i++) {
+					clients[i].sendUTF(JSON.stringify({
+						"index": index,
+						"requestType": "DELETE"
+					}))
+				}
+
 			}
 		}
 
